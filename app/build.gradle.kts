2feda765379c01026a877cab3ee3f366991dd66b
whoami <whoami@mailchat.me>
2026-08-17 22:18:30 +0530

    build: Add gradle.properties with android.useAndroidX=true and SDK config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,10 +49,6 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-
     buildFeatures {
         compose = true
         buildConfig = true
@@ -63,6 +59,10 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+}
+
+kotlin {
+    jvmToolchain(17)
 }
 
 dependencies {
